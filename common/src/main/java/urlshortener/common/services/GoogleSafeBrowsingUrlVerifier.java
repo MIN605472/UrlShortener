f433d8d011daed28ae15d0958056fff50ecb9191
Concatenacion <pedroforton@gmail.com>
2017-11-29 20:24:47 +0100

    Request to google. OK

--- common/src/main/java/urlshortener/common/services/GoogleSafeBrowsingUrlVerifier.java
+++ common/src/main/java/urlshortener/common/services/GoogleSafeBrowsingUrlVerifier.java
@@ -6,6 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import urlshortener.common.domain.GoogleSafeBrowsingConfig;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Map;
 
 public class GoogleSafeBrowsingUrlVerifier implements UrlVerifier {
@@ -31,17 +37,37 @@ public class GoogleSafeBrowsingUrlVerifier implements UrlVerifier {
 
             //"https://safebrowsing.googleapis.com/v4/threatLists?key=AIzaSyAj_XBSiZbcDjCA4003qdKHkfwov42JCIs"
 
-            ResponseEntity<String> response = restTemplate.postForEntity(
-                    "https://safebrowsing.googleapis.com/v4/threatLists?key=AIzaSyAj_XBSiZbcDjCA4003qdKHkfwov42JCIs",
-                    formParameters, String.class);
+            Client client = ClientBuilder.newClient();
 
-            System.out.println("Google Safe Browsing API returned HTTP Status: "+response.getStatusCode()+
-                    " and message: "+ response.getBody());
+            Response response = client.target("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyAj_XBSiZbcDjCA4003qdKHkfwov42JCIs")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.entity("{\n" +
+                            "    \"client\": {\n" +
+                            "      \"clientId\":      \"mountain-liquid\"\n" +
+                            "    },\n" +
+                            "    \"threatInfo\": {\n" +
+                            "      \"threatTypes\":      [\"MALWARE\", \"SOCIAL_ENGINEERING\", \"POTENTIALLY_HARMFUL_APPLICATION\", \"UNWANTED_SOFTWARE\"],\n" +
+                            "      \"platformTypes\":    [\"ALL_PLATFORMS\"],\n" +
+                            "      \"threatEntryTypes\": [\"URL\"],\n" +
+                            "      \"threatEntries\": [\n" +
+                            "        {\"url\": \"http://www.youtube.com/\"}\n" +
+                            "      ]\n" +
+                            "    }\n" +
+                            "  }",MediaType.APPLICATION_JSON));
 
-            if (response.getStatusCode() == HttpStatus.OK) {
-                System.out.print("Possible unsafe link: "+  url + " " + response.getBody());
-                isSafeUrl = false;
-            }
+            System.out.println(response);
+
+
+             //"https://safebrowsing.googleapis.com/v4/threatLists?key=AIzaSyAj_XBSiZbcDjCA4003qdKHkfwov42JCIs",
+
+
+            //System.out.println("Google Safe Browsing API returned HTTP Status: "+response.+
+            //        " and message: "+ response.getBody());
+
+            //if (response.getStatusCode() == HttpStatus.OK) {
+            //    System.out.print("Possible unsafe link: "+  url + " " + response.getBody());
+            //    isSafeUrl = false;
+            //}
 
         } catch (RestClientException restException) {
             System.out.print("Exception occurred during HTTP call to Google Safe Browsing API: " + restException);
