757a0fbe70bc27bf3406c998e29b8e0e588d9352
Sergio Andres Muniesa <sandresmuniesa@gmail.com>
2017-12-18 17:00:18 +0100

    Added Hystrix to URL validator

--- team/src/main/java/liquidmountain/Application.java
+++ team/src/main/java/liquidmountain/Application.java
@@ -1,7 +1,5 @@
 package liquidmountain;
 
-import liquidmountain.services.UrlValidatorAndChecker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
