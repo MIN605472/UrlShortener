435a9dde6096ae91494c69320dd3d739637c75f7
Sergio Andres Muniesa <sandresmuniesa@gmail.com>
2018-01-14 23:23:22 +0100

    More tests

--- team/src/test/java/liquidmountain/web/fixture/ClickFixture.java
+++ team/src/test/java/liquidmountain/web/fixture/ClickFixture.java
@@ -8,4 +8,14 @@ public class ClickFixture {
     public static Click click(ShortURL su) {
         return new Click(null, su.getHash(), null, null, null, null, null, null);
     }
+
+    public static Click clickPlatform(ShortURL su) {
+        return new Click(null, su.getHash(), null, null, null, "Windows 7", null, null);
+    }
+    public static Click clickBrowser(ShortURL su) {
+        return new Click(null, su.getHash(), null, null, "Mozilla Firefox", null, null, null);
+    }
+    public static Click clickCountry(ShortURL su) {
+        return new Click(null, su.getHash(), null, null, null, null, null, "Spain");
+    }
 }
